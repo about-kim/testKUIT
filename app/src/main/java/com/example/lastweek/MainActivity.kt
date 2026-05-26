3b0a39f1d2b39d36624f0f4a3aee3b2db1929239
about-kim <kimkorea2nd@gmail.com>
2026-05-27 00:45:31 +0900

    실습 진행

---
+++ app/src/main/java/com/example/lastweek/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.lastweek
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.feature.HomeScreen
+import com.example.lastweek.ui.theme.LastweekTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LastweekTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(
+                        name = "김대한",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LastweekTheme {
+        Greeting("Android")
+    }
+}
---
+++ feature/src/main/java/com/example/feature/HomeScreen.kt
@@ -0,0 +1,18 @@
+package com.example.feature
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.core.Greeter
+
+@Composable
+fun HomeScreen(modifier: Modifier, name: String) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            Greeter.hello(name)
+        )
+    }
+}
+
